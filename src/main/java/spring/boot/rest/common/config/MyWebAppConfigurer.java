8613895f41522306db181eeafd0f08eb4b9862ac
skywing <binyu0604@163.com>
2017-12-24 00:18:09 +0800

    丰东股份的

---
+++ src/main/java/spring/boot/rest/common/config/MyWebAppConfigurer.java
@@ -0,0 +1,16 @@
+package spring.boot.rest.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyWebAppConfigurer  extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://127.0.0.1")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
